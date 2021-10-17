7c97b7b7a585319e34068b0719176a29f94fd4c4
ben-altman <60247694+ben-altman@users.noreply.github.com>
2021-10-17 00:27:21 -0400

    commit tree 4c7c8eb4c129681c6dc9be3d72d65d7472e6ac05

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(ray) {
+    return ray.slice(0,2);
+};
+
+const returnLastTwoDrivers = function(ray) {
+    return ray.slice(-2);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function(value) {
+    return function(fare) {
+        return fare * value
+    };
+};
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function(ray, driverFunction) {
+    return driverFunction(ray);
+};
